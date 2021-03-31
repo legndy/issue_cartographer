3c3a43b64da1d36d11e994c0adfda70bcb9f18de
Xue Zhao Han <1468113168@qq.com>
2021-03-31 16:12:50 +0800

    issue when add imu

--- mapping.sh
+++ mapping.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 script_abs=$(readlink -f "${BASE_SOURCE-$0}")
 script_dir=$(dirname ${script_abs})
-. /opt/ros/kinetic/setup.sh
+. /opt/ros/melodic/setup.sh
 
-. ${script_dir}/install_isolated/setup.sh
+. ${script_dir}/devel_isolated/setup.sh
 
 roslaunch cartographer_ros rplidar_mapping.launch
 
--- matching.sh
+++ matching.sh
@@ -3,7 +3,7 @@ script_abs=$(readlink -f "${BASE_SOURCE-$0}")
 script_dir=$(dirname ${script_abs})
 . /opt/ros/melodic/setup.sh
 
-. ${script_dir}/install_isolated/setup.sh
+. ${script_dir}/devel_isolated/setup.sh
 
 roslaunch cartographer_ros rplidar_matching.launch load_state_filename:=${script_dir}/data/map.pbstream
 
